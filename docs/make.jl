efb22b400da7b4d220678b5c648da710604dcbe3
Jan Perina <perinja2@gmail.com>
2023-02-15 23:48:06 +0100

    add doc export

--- docs/make.jl
+++ docs/make.jl
@@ -1,21 +1,25 @@
-    using PredNets
-    using Documenter
+using PredNets
+using Documenter
 
-    cd("docs")
+cd("docs")
 
-    DocMeta.setdocmeta!(PredNets, :DocTestSetup, :(using PredNets); recursive=true)
+DocMeta.setdocmeta!(PredNets, :DocTestSetup, :(using PredNets); recursive=true)
 
-    makedocs(;
-        modules=[PredNets],
-        authors="Jan Perina <perinja2@gmail.com>",
-        repo="https://github.com/B0B36JUL-FinalProjects-2022/Projekt_perinja2/blob/{commit}{path}#{line}",
-        sitename="PredNets.jl",
-        format=Documenter.HTML(;
-            prettyurls=get(ENV, "CI", "false") == "true",
-            edit_link="main",
-            assets=String[],
-        ),
-        pages=[
-            "Home" => "index.md",
-        ],
-    )
+makedocs(;
+    modules=[PredNets],
+    authors="Jan Perina <perinja2@gmail.com>",
+    repo="https://github.com/B0B36JUL-FinalProjects-2022/Projekt_perinja2/blob/{commit}{path}#{line}",
+    sitename="PredNets.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(
+    repo = "github.com/B0B36JUL-FinalProjects-2022/Projekt_perinja2.git"
+)
